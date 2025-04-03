3e2c036f463c5ed7e68f163b52388d38ae4ccfb3
lemelinha <lucas.leme.0806@gmail.com>
2025-04-02 21:57:37 -0300

    ops

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -2,11 +2,11 @@
 <nav class="bg-indigo-700 text-white">
     <div class="container mx-auto px-4">
         <div class="flex space-x-4 py-2">
-            <a href="dashboard.html" class="py-2 px-4 bg-indigo-800 rounded">Dashboard</a>
-            <a href="books.html" class="py-2 px-4 hover:bg-indigo-800 rounded">Livros</a>
-            <a href="authors.html" class="py-2 px-4 hover:bg-indigo-800 rounded">Autores</a>
-            <a href="stock.html" class="py-2 px-4 hover:bg-indigo-800 rounded">Estoque</a>
-            <a href="users.html" class="py-2 px-4 hover:bg-indigo-800 rounded">Leitores</a>
+            <a href="#" class="py-2 px-4 bg-indigo-800 rounded">Dashboard</a>
+            <a href="#" class="py-2 px-4 hover:bg-indigo-800 rounded">Livros</a>
+            <a href="#" class="py-2 px-4 hover:bg-indigo-800 rounded">Autores</a>
+            <a href="#" class="py-2 px-4 hover:bg-indigo-800 rounded">Estoque</a>
+            <a href="#" class="py-2 px-4 hover:bg-indigo-800 rounded">Leitores</a>
         </div>
     </div>
 </nav>
